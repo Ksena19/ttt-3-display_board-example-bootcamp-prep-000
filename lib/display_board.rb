5fa391bca6d6ca0037aa253a7fc5c9cf404fbba8
Ksena <oksishukh@yahoo.com>
2017-12-24 22:25:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,4 +4,5 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "		 +    display += " #{value} "
   puts "-----------"		 +    display += (i+1) % 3 == 0 ? "\n-----------\n" : "|"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "		 +  end
-end		 +  puts display
+end		 
+  puts display
