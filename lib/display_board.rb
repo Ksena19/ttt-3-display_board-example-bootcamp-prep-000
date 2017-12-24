de07e4f6b88f9b9f0b6e93d60e82536391b5399c
Ksena <oksishukh@yahoo.com>
2017-12-24 22:25:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board)
- -  puts " #{board[0]} | #{board[1]} | #{board[2]} "		 +  display = ""
- -  puts "-----------"		 +  board.each_with_index do |value,i|
- -  puts " #{board[3]} | #{board[4]} | #{board[5]} "		 +    display += " #{value} "
- -  puts "-----------"		 +    display += (i+1) % 3 == 0 ? "\n-----------\n" : "|"
- -  puts " #{board[6]} | #{board[7]} | #{board[8]} "		 +  end
- -end		 +  puts display
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "		 +  display = ""
+  puts "-----------"		 +  board.each_with_index do |value,i|
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "		 +    display += " #{value} "
+  puts "-----------"		 +    display += (i+1) % 3 == 0 ? "\n-----------\n" : "|"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "		 +  end
+end		 +  puts display
